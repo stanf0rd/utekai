d1fadbac3762923eb2b18db7adaa4b8ca4f416c3
Dmitry Paliy <paliy@unim.su>
2020-06-04 23:49:47 +0300

    greeting structure refactored

---
+++ generator/generator.go
@@ -0,0 +1,22 @@
+package generator
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+// String generates pseudo-unique string
+// using standart a-zA-Z0-9 charset
+func String(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
--- main.go
+++ main.go
@@ -8,20 +8,17 @@ import (
 
 	tBot "gopkg.in/tucnak/telebot.v2"
 
+	"github.com/stanf0rd/utekai/generator"
 	"github.com/stanf0rd/utekai/sheets"
 )
 
 var bot *tBot.Bot
 var texts = map[string]string{
-	"next":                    "Далее",
-	"hello":                   "Привет! Мы клёвый бот, зовёмся паузой, Лиза классная, небо голубое.\n\n",
-	"anonimous_ask":           "Хошь анонимности?",
-	"anonimous_confirmed":     "Всё анонимно до чёртиков.",
-	"non_anonimous_confirmed": "Вася, записывай.",
-	"anonimous_notif":         "Ответы будут обезличены.",
-	"non_anonimous_notif":     "Художница увидит ваш ник.",
-	"notify_suggest":          "Включи уведомления, а то чо ты как лох. Чтобы бот и в могиле достучался.",
-	"announce_text":           "Ну пришлём чё-нить скоро, чо, жди.",
+	"anonimous_ask":           "Оставить художнице возможность связаться с вами?",
+	"greeting_text":           "Бот будет отправлять что-то иногда. Отвечай на вопросы. Можно фото, текст, чо попросят.",
+	"notify_suggest":          "Для большего вовлечения в процесс включи уведомления.",
+	"anonimous_confirmed":     "Ваши ответы будут обезличены.",
+	"non_anonimous_confirmed": "Художница увидит ваш ник и получит возможность связаться с вами.",
 }
 
 func main() {
@@ -36,10 +33,17 @@ func main() {
 	sheets.Authorize()
 	log.Println("Authorized to Google services")
 
-	bot.Handle("/start", setupStartCases())
+	bot.Handle("/start", createStart())
 
 	log.Println("Starting bot...")
 	bot.Start()
+}
+
+func createButton(text string) tBot.InlineButton {
+	return tBot.InlineButton{
+		Unique: generator.String(10),
+		Text:   text,
+	}
 }
 
 func initBot() (*tBot.Bot, error) {
@@ -53,102 +57,40 @@ func initBot() (*tBot.Bot, error) {
 	})
 }
 
-func setupStartCases() func(*tBot.Message) {
-	anonimous := tBot.InlineButton{
-		Unique: "anonimous",
-		Text:   "Yes",
-	}
-	nonAnonimous := tBot.InlineButton{
-		Unique: "non_anonimous",
-		Text:   "No",
+func createStart() func(*tBot.Message) {
+	createRules := func(anonymous bool) func(*tBot.Callback) {
+		return func(callback *tBot.Callback) {
+			printRules(callback, anonymous)
+		}
 	}
 
-	bot.Handle(&anonimous, createAnonymityChoiseProcessor(true))
-	bot.Handle(&nonAnonimous, createAnonymityChoiseProcessor(false))
+	return createAnonymityAsk(createRules)
+}
+
+func printRules(callback *tBot.Callback, anonymous bool) {
+	text := fmt.Sprintf("%s\n\n%s", texts["greeting_text"], texts["notify_suggest"])
+	if anonymous {
+		text = fmt.Sprintf("%s\n\n%s", text, texts["anonimous_confirmed"])
+	} else {
+		text = fmt.Sprintf("%s\n\n%s", text, texts["non_anonimous_confirmed"])
+	}
+
+	_, err := bot.Edit(callback.Message, text)
+	if err != nil {
+		log.Fatalln("Cannot edit message")
+	}
+}
+
+func createAnonymityAsk(createRules func(bool) func(*tBot.Callback)) func(*tBot.Message) {
+	nonAnonymous := createButton("Да")
+	anonymous := createButton("Нет")
+
+	bot.Handle(&anonymous, createRules(true))
+	bot.Handle(&nonAnonymous, createRules(false))
 
 	return func(message *tBot.Message) {
-		log.Print(fmt.Printf("/hello: sender %v", message.Sender))
-
-		bot.Send(message.Sender, texts["hello"]+texts["anonimous_ask"], &tBot.ReplyMarkup{
-			InlineKeyboard: [][]tBot.InlineButton{{anonimous, nonAnonimous}},
+		bot.Send(message.Sender, texts["anonimous_ask"], &tBot.ReplyMarkup{
+			InlineKeyboard: [][]tBot.InlineButton{{nonAnonymous, anonymous}},
 		})
-
-		// go sheets.Check()
-	}
-}
-
-func createAnonymityChoiseProcessor(anonimous bool) func(*tBot.Callback) {
-	var notifText, confirmText string
-	if anonimous {
-		notifText = texts["anonimous_notif"]
-		confirmText = texts["anonimous_confirmed"]
-	} else {
-		notifText = texts["non_anonimous_notif"]
-		confirmText = texts["non_anonimous_confirmed"]
-	}
-
-	nextButton := tBot.InlineButton{
-		Unique: "after_anonimity_next",
-		Text:   texts["next"],
-	}
-
-	suggestNotify := createNotifySuggester()
-	bot.Handle(&nextButton, func(callback *tBot.Callback) {
-		_, err := bot.Edit(callback.Message, texts["hello"]+confirmText)
-
-		if err != nil {
-			log.Fatalln("Cannot edit hello message", err)
-		}
-
-		suggestNotify(callback)
-	})
-
-	return func(callback *tBot.Callback) {
-		defer bot.Respond(callback, &tBot.CallbackResponse{
-			Text: notifText,
-		})
-
-		_, err := bot.Edit(callback.Message, texts["hello"]+confirmText, &tBot.ReplyMarkup{
-			InlineKeyboard: [][]tBot.InlineButton{{nextButton}},
-		})
-
-		if err != nil {
-			log.Fatalln("Cannot edit hello message", err)
-		}
-	}
-}
-
-func createNotifySuggester() func(*tBot.Callback) {
-	nextButton := tBot.InlineButton{
-		Unique: "after_suggest_next",
-		Text:   texts["next"],
-	}
-
-	bot.Handle(&nextButton, func(callback *tBot.Callback) {
-		_, err := bot.Edit(callback.Message, texts["notify_suggest"])
-
-		if err != nil {
-			log.Fatalln("Cannot edit hello message", err)
-		}
-
-		announce(callback)
-	})
-
-	return func(callback *tBot.Callback) {
-		_, err := bot.Send(callback.Sender, texts["notify_suggest"], &tBot.ReplyMarkup{
-			InlineKeyboard: [][]tBot.InlineButton{{nextButton}},
-		})
-
-		if err != nil {
-			log.Fatalln("Cannot send notify suggestion ", err)
-		}
-	}
-}
-
-func announce(callback *tBot.Callback) {
-	_, err := bot.Send(callback.Sender, texts["announce_text"])
-
-	if err != nil {
-		log.Fatalln("Cannot send announce text ", err)
 	}
 }
